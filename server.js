a2ad055b78969c442283c81b9dfae7d9d578243e
HueyWhyte <stanleykwaminaotabil@gmail.com>
2021-02-09 23:30:07 +0000

    remove strictness

--- server.js
+++ server.js
@@ -14,11 +14,14 @@ app.use("/api/student", require("./routes/studentRoutes"));
 // "mongodb://localhost:27017/hostel_management"
 // "mongodb+srv://HueyWhyte:Famous10@whyte.wdm4x.mongodb.net/whyte?retryWrites=true&w=majority"
 mongoose
-  .connect("mongodb://localhost:27017/hostel_management", {
-    useNewUrlParser: true,
-    useCreateIndex: true,
-    useUnifiedTopology: true,
-  })
+  .connect(
+    "mongodb+srv://HueyWhyte:Famous10@whyte.wdm4x.mongodb.net/whyte?retryWrites=true&w=majority",
+    {
+      useNewUrlParser: true,
+      useCreateIndex: true,
+      useUnifiedTopology: true,
+    }
+  )
   .then(() => console.log("Connected to MongoDB"))
   .catch((err) => console.log(err));
 
